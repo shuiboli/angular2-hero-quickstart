cf03d6d6a962f508934c4e326be329fb7f12dba7
tangyao <tangyao@hep.com.cn>
2016-12-01 15:32:38 +0800

    实现单向和双向的数据绑定。ngModel用于双向数据绑定,需要在app.module中先导入FormsModule模块

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,25 @@
 import { Component } from '@angular/core';
+export class Hero {
+  id: number;
+  name: string;
+}
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>{{title}}</h1>
+             <h2>{{hero.name}} details</h2>
+             <div><label>id:</label>{{hero.id}}</div>
+             <div>
+                <label>name:</label>
+                <!--ngModel用于双向数据绑定,需要在app.module中先导入FormsModule模块-->
+                <input [(ngModel)]="hero.name" placeholder="name">  
+            </div>
+  `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { 
+  title='Tour of Heroes';
+  hero : Hero = {
+    id:1,
+    name:'Windstorm'
+  };
+}
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms'; //使得表单包支持双向数据绑定
 
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
